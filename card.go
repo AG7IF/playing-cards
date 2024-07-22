d22a4e32080bf3ccc4c71fc8cec39cd676b418f5
Robert Hawk <robert.hawk@gmail.com>
2024-07-22 08:54:33 -0600

    fix: Added LaTeX() method back onto Card

--- card.go
+++ card.go
@@ -1,6 +1,14 @@
 package playing_cards
 
+import (
+	"fmt"
+)
+
 type Card struct {
 	Suit Suit
 	Rank Rank
+}
+
+func (c Card) LaTeX() string {
+	return fmt.Sprintf("%s%s", c.Rank.LaTeX(), c.Suit.LaTeX())
 }
